43086750eeef656e5ccd9edf6e12a74060248526
vegabryam <vegabryam40@gmail.com>
2019-05-29 00:42:27 -0500

    Agregando API Key

--- vista/user/vista/crear_evento.php
+++ vista/user/vista/crear_evento.php
@@ -15,7 +15,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
             crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=API KEY"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoJ3ujl8XgJZMJ3H8Hfu4wXa41tY_Eozc"></script>
     <script type="text/javascript">
         function initialize() {
             // Creating map object
