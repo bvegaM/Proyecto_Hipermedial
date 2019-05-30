670d46763b45f4ba8a8b3c83341c20ad1e93cfc7
vegabryam <vegabryam40@gmail.com>
2019-05-30 15:48:52 -0500

    Ajax

--- vista/admin/controladores/php/buscarE.php
+++ vista/admin/controladores/php/buscarE.php
@@ -50,32 +50,30 @@
 		
 		$sql1="SELECT *
 			  FROM T_EMPRESAS
-			  WHERE emp_nombres like '%$evento%'";
+			  WHERE emp_nombre like '%$evento%'";
 		
 		$result1 = $conn->query($sql1);
 ?>
-<div class="detalle" style="background:white;">
+ <div class="detalle" style="background:white;">
                    	<table style="width:100%; color:black; text-align:center;">
                    		<tr>
-                   			<th style="border-bottom:1px solid black;">Nombre del Evento</th>
-                   			<th style="border-bottom:1px solid black;">Fecha del Evento</th>
-                   			<th style="border-bottom:1px solid black;">Descripcion</th>
-                   			<th style="border-bottom:1px solid black;">Cantidad</th>
-                   			<th style="border-bottom:1px solid black;">Total</th>
-                   			<th style="border-bottom:1px solid black;">Anular Factura</th>
+                   			<th style="border-bottom:1px solid black;">Nombre de la empresa</th>
+                   			<th style="border-bottom:1px solid black;">RUC</th>
+                   			<th style="border-bottom:1px solid black;">Direccion</th>
+                   			<th style="border-bottom:1px solid black;">Telefono</th>
+                   			<th style="border-bottom:1px solid black;">Eliminar Empresa</th>
                    		</tr>
                    		<?php
 							while($row1 = $result1->fetch_assoc()){
 								echo "<tr class='datos'>";
-									echo "<td>".$row1["evt_desc"]."</td>";
-									echo "<td>".$row1["evt_fec_evento"]."</td>";
-									echo "<td>".$row1["fd_desc"]."</td>";
-									echo "<td>".$row1["fd_cantidad"]."</td>";
-									echo "<td>$".$row1["fd_precio"]."</td>";
-									if($row1["fd_estado"] == 'N'){
-										echo "<td class='link_compra'><a href='../controladores/php/anular_factura.php?fc=".$row1["fc_id"]."&fd=".$row1["fd_id"]."&codigo=".$codigo."'><i class='fas fa-trash-alt' style='color:red;'></i></a></td>";
+									echo "<td>".$row1["emp_nombre"]."</td>";
+									echo "<td>".$row1["emp_ruc"]."</td>";
+									echo "<td>".$row1["emp_direccion"]."</td>";
+									echo "<td>".$row1["emp_telefono"]."</td>";
+									if($row1["emp_estado_elimina"] == 'N'){
+										echo "<td class='link_compra'><a href='../controladores/php/eliminar_empresa.php?emp=".$row1["emp_id"]."&codigo=".$codigo."'><i class='fas fa-trash-alt' style='color:red;'></i></a></td>";
 									}else{
-										echo "<td>ANULADA</td>";
+										echo "<td>ELIMINADA</td>";
 									}
 									
 								echo "</tr>";
