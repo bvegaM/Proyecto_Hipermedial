4e5e9f0afa249411912b01625ba94fad38ed9a2f
vegabryam <vegabryam40@gmail.com>
2019-05-29 21:58:36 -0500

    creando resumen_compra.php;

--- vista/user/controladores/js/funciones.js
+++ vista/user/controladores/js/funciones.js
@@ -4,3 +4,8 @@ btnMenu.addEventListener('click',function(){
 	'use strict';
 	menu.classList.toggle('mostrar');
 });
+function cambiarCantidad(){
+	var c = Number(document.getElementById("cant").value)
+	alert(c)
+}
+	
