5f9f9a235f7f5dba8262c8aef6a8045c550eba2a
vegabryam <vegabryam40@gmail.com>
2019-05-29 20:42:44 -0500

    creando resumen_compra.php;

--- vista/user/vista/resumen_compra.php
+++ vista/user/vista/resumen_compra.php
@@ -20,6 +20,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Evento</title>
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles_evento.css">
 </head>
