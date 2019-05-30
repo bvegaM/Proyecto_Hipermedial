98a666306e997f94797fee1573d4392ae5284e15
vegabryam <vegabryam40@gmail.com>
2019-05-30 01:58:05 -0500

    Creando pagina mis_compras.php

--- vista/user/controladores/php/crear_factura.php
+++ vista/user/controladores/php/crear_factura.php
@@ -12,29 +12,29 @@
 	$cantidad=isset($_POST["cnt"])?trim($_POST["cnt"]):null;
 	$fechaC = date("y-m-d h:i:s",time());
 
-	$sql="INSERT INTO T_FACTURA_CABECERA VALUES(0,'$fechaC',$codigo)";
+	$sql="INSERT INTO T_FACTURA_CABECERA VALUES(0,'$fechaC','N',$codigo)";
 	$result =$conn->query($sql);
 	if($eventoId == 1){
 		$id = $conn->insert_id;
-		$facturaD = "INSERT INTO T_FACTURA_DETALLE VALUES(0,'BOLETO PARA GENERAL',$cantidad,$precio,$id,$evento)";
+		$facturaD = "INSERT INTO T_FACTURA_DETALLE VALUES(0,'BOLETO PARA GENERAL',$cantidad,$precio,'N',$id,$evento)";
 		$resultFD = $conn->query($facturaD);
 		header("Location: ../../vista/mis_compras.php?codigo=".$codigo);
 	}
 	if($eventoId == 2){
 		$id = $conn->insert_id;
-		$facturaD = "INSERT INTO T_FACTURA_DETALLE VALUES(0,'BOLETO PARA TRIBUNA',$cantidad,$precio,$id,$evento)";
+		$facturaD = "INSERT INTO T_FACTURA_DETALLE VALUES(0,'BOLETO PARA TRIBUNA',$cantidad,$precio,'N',$id,$evento)";
 		$resultFD = $conn->query($facturaD);
 		header("Location: ../../vista/mis_compras.php?codigo=".$codigo);
 	}
 	if($eventoId == 3){
 		$id = $conn->insert_id;
-		$facturaD = "INSERT INTO T_FACTURA_DETALLE VALUES(0,'BOLETO PARA VIP',$cantidad,$precio,$id,$evento)";
+		$facturaD = "INSERT INTO T_FACTURA_DETALLE VALUES(0,'BOLETO PARA VIP',$cantidad,$precio,'N',$id,$evento)";
 		$resultFD = $conn->query($facturaD);
 		header("Location: ../../vista/mis_compras.php?codigo=".$codigo);
 	}
 	if($eventoId == 4){
 		$id = $conn->insert_id;
-		$facturaD = "INSERT INTO T_FACTURA_DETALLE VALUES(0,'BOLETO PARA BOX',$cantidad,$precio,$id,$evento)";
+		$facturaD = "INSERT INTO T_FACTURA_DETALLE VALUES(0,'BOLETO PARA BOX',$cantidad,$precio,'N',$id,$evento)";
 		$resultFD = $conn->query($facturaD);
 		header("Location: ../../vista/mis_compras.php?codigo=".$codigo);
 	}
