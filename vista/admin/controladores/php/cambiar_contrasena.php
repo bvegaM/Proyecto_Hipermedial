10a05815a6105262c8dcaf110a2005dbc1175d7e
vegabryam <vegabryam40@gmail.com>
2019-05-30 05:22:25 -0500

    creando contrasena admin

---
+++ vista/admin/controladores/php/cambiar_contrasena.php
@@ -0,0 +1,18 @@
+<?php
+	
+	include '../../../../config/conexion.php';
+
+	$codigo =isset($_POST["codigo"])?trim($_POST["codigo"]):null;
+	$contrasena = isset($_POST["rcontrasena"]) ? trim($_POST["rcontrasena"]) : null;
+
+	echo $codigo;
+	echo $contrasena;
+	$sql ="UPDATE T_USUARIOS
+		  SET usu_contrasena = MD5('$contrasena')
+		  WHERE usu_id = $codigo";
+
+	$result = $conn->query($sql);
+
+	header("Location: cerrar_sesion.php");
+
+?>
