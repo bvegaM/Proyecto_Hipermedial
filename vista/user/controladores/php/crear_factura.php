8b12bf686d8549f66eb8b8319ec0a2564c2faf39
vegabryam <vegabryam40@gmail.com>
2019-05-30 00:19:16 -0500

    creando crear_factura.php;

--- vista/user/controladores/php/crear_factura.php
+++ vista/user/controladores/php/crear_factura.php
@@ -17,5 +17,49 @@
 
 	echo $sql;
 
+	if($eventoId == 1){
+		$factura="SELECT *
+				  FROM T_FACTURA_CABECERA 
+				  WHERE fc_usu_id=$codigo AND
+				  fc_fecha='$fechaC'";
+		
+		$resultadoF=$conn->query($factura);
+		$row=$resultadoF->fetch_assoc();
+		$sql1="INSERT INTO T_FACTURA_DETALLE VALUES(0,'boleto General',$cantidad,$precio,$row['fc_id'],$evento)";
+		$result1 =$conn->query($sql1);
+	}
+	if($eventoId == 2){
+		$factura="SELECT *
+				  FROM T_FACTURA_CABECERA 
+				  WHERE fc_usu_id=$codigo AND
+				  fc_fecha='$fechaC'";
+		
+		$resultadoF=$conn->query($factura);
+		$row=$resultadoF->fetch_assoc();
+		$sql1="INSERT INTO T_FACTURA_DETALLE VALUES(0,'boleto Tribuna',$cantidad,$precio,$row['fc_id'],$evento)";
+		$result1 =$conn->query($sql1);
+	}
+	if($eventoId == 3){
+		$factura="SELECT *
+				  FROM T_FACTURA_CABECERA 
+				  WHERE fc_usu_id=$codigo AND
+				  fc_fecha='$fechaC'";
+		
+		$resultadoF=$conn->query($factura);
+		$row=$resultadoF->fetch_assoc();
+		$sql1="INSERT INTO T_FACTURA_DETALLE VALUES(0,'boleto VIP',$cantidad,$precio,$row['fc_id'],$evento)";
+		$result1 =$conn->query($sql1);
+	}
+	if($eventoId == 4){
+		$factura="SELECT *
+				  FROM T_FACTURA_CABECERA 
+				  WHERE fc_usu_id=$codigo AND
+				  fc_fecha='$fechaC'";
+		
+		$resultadoF=$conn->query($factura);
+		$row=$resultadoF->fetch_assoc();
+		$sql1="INSERT INTO T_FACTURA_DETALLE VALUES(0,'boleto BOX',$cantidad,$precio,$row['fc_id'],$evento)";
+		$result1 =$conn->query($sql1);
+	}
 	
 ?>
