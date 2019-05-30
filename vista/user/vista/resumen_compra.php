625726f419c7d9869d67e6354b3d278800077b43
vegabryam <vegabryam40@gmail.com>
2019-05-29 20:41:54 -0500

    creando resumen_compra.php;

--- vista/user/vista/resumen_compra.php
+++ vista/user/vista/resumen_compra.php
@@ -1,7 +1,97 @@
 <?php
- $evento = $_GET["evt"];
- $eventoId = $_GET["evtId"];
- $codigo = $_GET["codigo"];
-
-
+    session_start();
+    if(!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE){
+        header("Location: ../../../public/vista/login.html");
+    }
 ?>
+ <?php
+		$codigo = $_GET["codigo"];
+		include '../../../config/conexion.php';
+		
+		$sql="SELECT *
+			  FROM T_USUARIOS
+			  WHERE usu_id = $codigo";
+		
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+	?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Evento</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles_evento.css">
+</head>
+<body>  
+ 	<header class="header">
+		<div class="cabecera">
+			<h1 class="logo"><i class="fas fa-ticket-alt"></i>Ticket Home</h1>
+	
+			<div class="info">
+				<i class="fas fa-phone"><span>09-85164142</span></i>
+				<i class="fas fa-map-marker-alt"><span>Mall del Rio Planta Baja</span></i>
+			</div>
+		</div>
+	</header>
+	<nav class="navegador">
+		<div class="menu_despegable">
+			<i class="fas fa-bars" id="btnmenu"></i>
+			<ul class="menu" id="menu">
+				<li class="menu__item"><a href="index.php?codigo=<?php echo $codigo?>" class="menu__link menu__link--select"><i class="fas fa-home"><span>Home</span></i></a></li>
+				<li class="menu__item"><a href="perfil.php?codigo=<?php echo $codigo?>" class="menu__link"><i class="fas fa-user-edit"><span>Mi Perfil</span></i></a></li>
+				<li class="menu__item"><a href="evento.php?codigo=<?php echo $codigo?>" class="menu__link"><i class="fas fa-ticket-alt"><span>Comprar</span></i></a></li>
+				<li class="menu__item"><a href="crear_evento.php?codigo=<?php echo $codigo?>" class="menu__link"><i class="far fa-plus-square"><span>Crear Evento</span></i></a></li>
+				<li class="menu__item"><a href="contact.html" class="menu__link"><i class="fas fa-money-check-alt"><span>Mis Compras</span></i></a></li>
+			</ul>
+		</div>
+		<div class="rol">
+			<a href="perfil.php?codigo=<?php echo $codigo?>" class="sesion"><i class="fas fa-smile-beam"><span>Bienvenido <?php echo $row["usu_nombres"]; ?></span></i></a>
+			<a href="../controladores/php/cerrar_sesion.php" class="sesion"><i class="fas fa-sign-in-alt" id="inicio"><span>Cerrar Sesión</span></i></a>
+		</div>
+	</nav> 
+    <div class="content">
+        <h1 class="logo">Crear <span>Evento</span></h1>
+        <div class="contact-wrapper">
+            <div class="contact-form">
+                <h3>Paso 1: Crea tu Empresa u Organizador (Si ya lo registraste lo puedes encontrar)</h3>
+                <form action="../controladores/php/crear_empresa.php" method="post" onsubmit="return validar()" enctype="multipart/form-data">
+                    <p class="block">
+                        <input type="submit" value="Registrar" class="button" id="botonA">
+                        <input type="reset" value="Cancelar" class="button" onclick="limpiar()" id="botonB">
+                    </p>
+                </form>
+            </div>
+            <div class="contact-info">
+                <h3>Paso 2: Crea tu Evento para que este disponible </h3>
+                <form onsubmit="return validar()" enctype="multipart/form-data">
+                   
+                </form>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+		<div class="footer-social-icons">
+			<ul>
+				<li><a href="" target="blank"><i class="fab fa-facebook-square"></i>
+				</a></li>
+				<li><a href="" target="blank"><i class="fab fa-instagram"></i></a></li>
+				<li><a href="" target="blank"><i class="fab fa-github"></i></a></li>
+			</ul>
+		</div>
+		<div class="footer-menu-one">
+			<ul>
+				<li><a href="index.php?codigo=<?php echo $codigo?>">Home</a></li>
+				<li><a href="perfil.php?codigo=<?php echo $codigo?>">Mi Perfil</a></li>
+				<li><a href="evento.php?codigo=<?php echo $codigo?>">Comprar</a></li>
+				<li><a href="#">Crear Eventos</a></li>
+				<li><a href="">Mis Compras</a></li>
+			</ul>
+		</div>
+		<div class="footer-txt">
+			<p> &copy; Todos Los Derechos Reservados Universidad Politécnica Salesiana</p>
+			<p id="f-min">Cuenca-Ecuador</p>
+		</div>
+	</footer>
+</body>
+</html>
