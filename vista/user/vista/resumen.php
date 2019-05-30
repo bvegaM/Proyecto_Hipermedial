d669f470354f9f4100ad0b45af1e623e92ebc2a4
vegabryam <vegabryam40@gmail.com>
2019-05-29 19:21:54 -0500

    Agregando tabla a resumen

--- vista/user/vista/resumen.php
+++ vista/user/vista/resumen.php
@@ -119,6 +119,14 @@
 			<?php echo "<img class='img_event' src='data:".$row1['evt_img_tipo']."; base64,".base64_encode($row1['evt_img'])."'>"; ?>
 		</section>
 		<div id="map_canvas" style="width: 50%; height: 400px;" class="block"></div>
+		<table border="1">
+			<tr>
+				<th>Tipo</th>
+				<th>Precio</th>
+				<th>Asientos</th>
+				<th>Comprar</th>
+			</tr>
+		</table>
 	</main>
 	<footer class="footer">
 		<div class="footer-social-icons">
