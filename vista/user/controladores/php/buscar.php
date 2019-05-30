4252ece21ab4d5f2ed30ad841816bfff50f27df9
vegabryam <vegabryam40@gmail.com>
2019-05-30 15:43:38 -0500

    Ajax

--- vista/user/controladores/php/buscar.php
+++ vista/user/controladores/php/buscar.php
@@ -17,56 +17,35 @@
     </style>
 </head>
 <body>
-<?php
-	include '../../../../config/conexion.php';
+<section class="grupo_eventos grupo_event_musica">
+	<div class="eventos">
+		 <?php
+            include '../../../../config/conexion.php';
             $evento = $_GET['evt'];
 			$codigo = $_GET['usu'];
-	
-			$sql="SELECT *
-			  FROM T_USUARIOS
-			  WHERE usu_id = $codigo";
-		
-		$result = $conn->query($sql);
-		$row = $result->fetch_assoc();
-		
-		$sql1="SELECT *
-			  FROM T_FACTURA_CABECERA,
-			  	   T_FACTURA_DETALLE,
-				   T_EVENTOS
-			  WHERE fc_id = fd_fc_id AND
-			  		evt_id = fd_evt_id AND
-					evt_desc like '%$evento%'";
-		
-		$result1 = $conn->query($sql1);
-?>
-<div class="detalle" style="background:white;">
-                   	<table style="width:100%; color:black; text-align:center;">
-                   		<tr>
-                   			<th style="border-bottom:1px solid black;">Nombre del Evento</th>
-                   			<th style="border-bottom:1px solid black;">Fecha del Evento</th>
-                   			<th style="border-bottom:1px solid black;">Descripcion</th>
-                   			<th style="border-bottom:1px solid black;">Cantidad</th>
-                   			<th style="border-bottom:1px solid black;">Total</th>
-                   			<th style="border-bottom:1px solid black;">Anular Factura</th>
-                   		</tr>
-                   		<?php
-							while($row1 = $result1->fetch_assoc()){
-								echo "<tr class='datos'>";
-									echo "<td>".$row1["evt_desc"]."</td>";
-									echo "<td>".$row1["evt_fec_evento"]."</td>";
-									echo "<td>".$row1["fd_desc"]."</td>";
-									echo "<td>".$row1["fd_cantidad"]."</td>";
-									echo "<td>$".$row1["fd_precio"]."</td>";
-									if($row1["fd_estado"] == 'N'){
-										echo "<td class='link_compra'><a href='../controladores/php/anular_factura.php?fc=".$row1["fc_id"]."&fd=".$row1["fd_id"]."&codigo=".$codigo."'><i class='fas fa-trash-alt' style='color:red;'></i></a></td>";
-									}else{
-										echo "<td>ANULADA</td>";
-									}
-									
-								echo "</tr>";
-							}
-						?>
-                   	</table>
-                   </div>
+            $sql ="SELECT *
+                FROM T_EVENTOS,
+					 T_EMPRESAS
+				WHERE evt_desc like '%$evento%' AND
+					  evt_estado_elimina ='N' AND
+					  emp_id=evt_emp_id AND
+					  emp_estado_elimina='N'";
+                $result = $conn->query($sql);
+                if($result->num_rows > 0){
+                         while($row = $result->fetch_assoc()){
+                            echo "<div class='column_event'>";
+								echo "<img class='img_event' src='data:".$row['evt_img_tipo']."; base64,".base64_encode($row['evt_img'])."'>";
+					 			echo "<h4 class='title_event'>".$row["evt_desc"]."</h4>";
+					 			echo "<p>".$row['evt_fec_evento']."</p>";
+					 			echo "<a href='resumen.php?evt=".$row["evt_id"]."&codigo=".$codigo."' class='link_event'>Ir al Evento</a>";
+							echo "</div>";
+                         }  
+                }else{
+                }      
+        ?>
+	</div>
+</section>
+
+
 </body>
 </html>
