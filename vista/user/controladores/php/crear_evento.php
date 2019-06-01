1112e79d8800864e2c2c965951721e736334df50
vegabryam <vegabryam40@gmail.com>
2019-05-31 20:18:30 -0500

    Arreglando creacion de evento

--- vista/user/controladores/php/crear_evento.php
+++ vista/user/controladores/php/crear_evento.php
@@ -35,9 +35,9 @@
     $contenido = addslashes($contenido);
     fclose($archivo_objetivo);
 
-	$sql = "INSERT INTO T_EVENTOS VALUES(0,'$nombres','$fecha','$direccion',$latitud,$longitud,'$contenido','$tipo_archivo','D',0,'admin','$fechaC',null,$empresa)";
+	$sql = "INSERT INTO T_EVENTOS VALUES(0,'$nombres',STR_TO_DATE(REPLACE('$fecha','/','.') ,GET_FORMAT(date,'EUR')),'$direccion',$latitud,$longitud,'$contenido','$tipo_archivo','D',0,'admin','$fechaC',null,$empresa)";
 	$id = $conn->insert_id;
-	if($conn->query($sql) === TRUE){
+
 	if($conn->query($sql) === TRUE){
 		if($aG!="" && $pG !=""){
 			$sql1="INSERT INTO T_EVENTOS_ASIENTOS VALUES(0,$pG,$aG,$id,1)";
