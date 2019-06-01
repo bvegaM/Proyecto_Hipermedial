330daaab31c6216007c7558988f91393cd96be88
vegabryam <vegabryam40@gmail.com>
2019-05-31 20:03:53 -0500

    Arreglando creacion de evento

--- vista/user/controladores/php/crear_evento.php
+++ vista/user/controladores/php/crear_evento.php
@@ -4,42 +4,40 @@
 	date_default_timezone_set("America/Guayaquil");	
 
 	$id = isset($_POST["id"]) ? trim($_POST["id"]): null;
+
+	//DATOS PARA CREAR EVENTO
 	$fechaC = date("y-m-d h:i:s",time());
 	$nombres =isset($_POST["nombres"]) ? mb_strtoupper(trim($_POST["nombres"]),"UTF-8"): null;
 	$direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]),"UTF-8"):null;
 	$fecha = isset($_POST["fecha"])? trim($_POST["fecha"]): null;
-	$aG = isset($_POST["genA"]) ? trim($_POST["genA"]): null;
-	$aT = isset($_POST["tribA"]) ? trim($_POST["tribA"]): null;
-	$aV = isset($_POST["vipA"]) ? trim($_POST["vipA"]): null;
-	$aB = isset($_POST["boxA"]) ? trim($_POST["boxA"]): null;
-	$pG = isset($_POST["genP"]) ? trim($_POST["genP"]): null;
-	$pT = isset($_POST["tribP"]) ? trim($_POST["tribP"]): null;
-	$pV = isset($_POST["vipP"]) ? trim($_POST["vipP"]): null;
-	$pB = isset($_POST["boxP"]) ? trim($_POST["boxP"]): null;
 	$empresa = isset($_POST["emp"])?trim($_POST["emp"]):null;
 	$nombre_archivo = $_FILES['imagenUpdate']['name'];
     $tipo_archivo = $_FILES['imagenUpdate']['type'];
     $tamano_archivo = $_FILES['imagenUpdate']['size'];
+	//PARA EL MAPA
 	$latitud =  isset($_POST["latitud"])?trim($_POST["latitud"]):null;
 	$longitud =  isset($_POST["longitud"])?trim($_POST["longitud"]):null;
-
+	//ASIENTOS PARA EL EVENTO
+	$aG = isset($_POST["genA"]) ? trim($_POST["genA"]): null;
+	$aT = isset($_POST["tribA"]) ? trim($_POST["tribA"]): null;
+	$aP = isset($_POST["palA"]) ? trim($_POST["palA"]): null;
+	$aV = isset($_POST["vipA"]) ? trim($_POST["vipA"]): null;
+	$aB = isset($_POST["boxA"]) ? trim($_POST["boxA"]): null;
+	$pG = isset($_POST["genP"]) ? trim($_POST["genP"]): null;
+	$pT = isset($_POST["tribP"]) ? trim($_POST["tribP"]): null;
+	$pP = isset($_POST["palP"]) ? trim($_POST["palP"]): null;
+	$pV = isset($_POST["vipP"]) ? trim($_POST["vipP"]): null;
+	$pB = isset($_POST["boxP"]) ? trim($_POST["boxP"]): null;
+	
+	//IMAGEN
     $archivo_objetivo = fopen($_FILES['imagenUpdate']['tmp_name'],'r');
     $contenido=fread($archivo_objetivo,$tamano_archivo);
     $contenido = addslashes($contenido);
     fclose($archivo_objetivo);
 
-	echo $aG;
-	echo $pG;
-	echo $aT;
-	echo $pT;
-	echo $aV;
-	echo $pV;
-	echo $aB;
-	echo $pB;
 
-	if($aT == ""){
-		echo "hola";
-	}
+	echo $longitud;
+	echo $latitud;
 	
 
 ?>
