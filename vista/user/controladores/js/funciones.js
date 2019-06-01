39cd668e762acd2336f2c259a41f38f9ac06946f
vegabryam <vegabryam40@gmail.com>
2019-06-01 00:29:53 -0500

    Arreglando creacion de evento

--- vista/user/controladores/js/funciones.js
+++ vista/user/controladores/js/funciones.js
@@ -10,18 +10,23 @@ function cambiarCantidad(){
 	
 	if(a == 1){
 		document.getElementById("c1").innerHTML=c
+		document.getElementById("d1").style.visibility="visible";
 	}
 	if(a == 2){
 		document.getElementById("c2").innerHTML=c
+		document.getElementById("d2").style.visibility="visible";
 	}
 	if(a == 3){
 		document.getElementById("c3").innerHTML=c
+		document.getElementById("d3").style.visibility="visible";
 	}
 	if(a == 4){
 		document.getElementById("c4").innerHTML=c
+		document.getElementById("d4").style.visibility="visible";
 	}
 	if(a == 5){
 		document.getElementById("c5").innerHTML=c
+		document.getElementById("d5").style.visibility="visible";
 	}
 }
 function cambio(){
