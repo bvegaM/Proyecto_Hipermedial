8ff6c59a62d1b6d21f2527f4e2b06b8027ea879e
vegabryam <vegabryam40@gmail.com>
2019-06-02 17:01:56 -0500

    creando js

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+function buscarPorEvento(){
+    var evento = document.getElementById("evento").value
+	var usuario = document.getElementById("usuario").value
+    if(evento ==""){
+        document.getElementById("eventos").innerHTML="";
+    }else{
+        if(window.XMLHttpRequest){
+            xmlhttp = new XMLHttpRequest();
+        }else{
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                document.getElementById("eventos").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","../controladores/php/buscar.php?evt="+evento+"&usu="+usuario,true);
+        xmlhttp.send();
+    }
+}
