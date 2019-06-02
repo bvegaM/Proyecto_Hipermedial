6c1a7cb4846d91a4f240380c80e6bae4aedf9abc
vegabryam <vegabryam40@gmail.com>
2019-06-02 12:31:17 -0500

    modificando resumen_compra

--- vista/user/controladores/js/funciones.js
+++ vista/user/controladores/js/funciones.js
@@ -12,34 +12,34 @@ function cambiarCantidad(){
 		var pu = Number(document.getElementById("pU1").value)
 		var pT = pu*c
 		document.getElementById("c1").innerHTML=c
-		document.getElementById("p1").innerHTML=pT
+		document.getElementById("p1").innerHTML="$"+pT
 		document.getElementById("d1").style.visibility="visible";
 	}
 	if(a == 2){
 		var pu = Number(document.getElementById("pU2").value)
 		var pT = pu*c
-		document.getElementById("p2").innerHTML=pT
+		document.getElementById("p2").innerHTML="$"+pT
 		document.getElementById("c2").innerHTML=c
 		document.getElementById("d2").style.visibility="visible";
 	}
 	if(a == 3){
 		var pu = Number(document.getElementById("pU3").value)
 		var pT = pu*c
-		document.getElementById("p3").innerHTML=pT
+		document.getElementById("p3").innerHTML="$"+pT
 		document.getElementById("c3").innerHTML=c
 		document.getElementById("d3").style.visibility="visible";
 	}
 	if(a == 4){
 		var pu = Number(document.getElementById("pU4").value)
 		var pT = pu*c
-		document.getElementById("p4").innerHTML=pT
+		document.getElementById("p4").innerHTML="$"+pT
 		document.getElementById("c4").innerHTML=c
 		document.getElementById("d4").style.visibility="visible";
 	}
 	if(a == 5){
 		var pu = Number(document.getElementById("pU5").value)
 		var pT = pu*c
-		document.getElementById("p5").innerHTML=pT
+		document.getElementById("p5").innerHTML="$"+pT
 		document.getElementById("c5").innerHTML=c
 		document.getElementById("d5").style.visibility="visible";
 	}
