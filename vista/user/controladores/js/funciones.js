629a2e76342f793f6e421969f4e7b24009d08501
vegabryam <vegabryam40@gmail.com>
2019-06-02 12:37:20 -0500

    modificando resumen_compra

--- vista/user/controladores/js/funciones.js
+++ vista/user/controladores/js/funciones.js
@@ -8,9 +8,11 @@ function cambiarCantidad(){
 	var c = Number(document.getElementById("cant").value)
 	var a = Number(document.getElementById("ast").value)
 	var t=  0
+	var t1=0,t2=0,t3=0,t4=0,t5=0
 	if(a == 1){
 		var pu = Number(document.getElementById("pU1").value)
 		var pT = pu*c
+		t1 = pT
 		document.getElementById("c1").innerHTML=c
 		document.getElementById("p1").innerHTML="$"+pT
 		document.getElementById("d1").style.visibility="visible";
@@ -18,6 +20,7 @@ function cambiarCantidad(){
 	if(a == 2){
 		var pu = Number(document.getElementById("pU2").value)
 		var pT = pu*c
+		t2 = pT
 		document.getElementById("p2").innerHTML="$"+pT
 		document.getElementById("c2").innerHTML=c
 		document.getElementById("d2").style.visibility="visible";
@@ -25,6 +28,7 @@ function cambiarCantidad(){
 	if(a == 3){
 		var pu = Number(document.getElementById("pU3").value)
 		var pT = pu*c
+		t3=pT
 		document.getElementById("p3").innerHTML="$"+pT
 		document.getElementById("c3").innerHTML=c
 		document.getElementById("d3").style.visibility="visible";
@@ -32,6 +36,7 @@ function cambiarCantidad(){
 	if(a == 4){
 		var pu = Number(document.getElementById("pU4").value)
 		var pT = pu*c
+		t4 = pT
 		document.getElementById("p4").innerHTML="$"+pT
 		document.getElementById("c4").innerHTML=c
 		document.getElementById("d4").style.visibility="visible";
@@ -39,10 +44,13 @@ function cambiarCantidad(){
 	if(a == 5){
 		var pu = Number(document.getElementById("pU5").value)
 		var pT = pu*c
+		t5= pT
 		document.getElementById("p5").innerHTML="$"+pT
 		document.getElementById("c5").innerHTML=c
 		document.getElementById("d5").style.visibility="visible";
 	}
+	t=t1+t2+t3+t4+t5
+	document.getElementById("total").innetHTML="$"+t;
 }
 function cambio(){
 	document.getElementById("cant").value=0
