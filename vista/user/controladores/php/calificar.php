163ac13c250b7ed3df32a33f44c9b39044122bf1
vegabryam <vegabryam40@gmail.com>
2019-06-03 06:25:20 -0500

    calificacion

---
+++ vista/user/controladores/php/calificar.php
@@ -0,0 +1,13 @@
+<?php
+	include '../../../../config/conexion.php';
+	$estado = isset($_POST["cal"]) ? trim($_POST["cal"]): null;
+	$cali = isset($_POST["cali"]) ? trim($_POST["cali"]): null;
+	$idcal = isset($_POST["idcal"]) ? trim($_POST["idcal"]): null;
+	$codigo = isset($_POST["user"]) ? trim($_POST["user"]): null;
+	$promedio = ($cali+$estado)/2;
+	$sql = "UPDATE T_EVENTOS
+			SET evt_calificacion = $promedio
+			WHERE evt_id = $idcal";
+	$result = $conn->query($sql);
+	header("location: ../../vista/resumen.php?codigo=".$id."&evt=".$idcal);
+?>
--- vista/user/vista/resumen.php
+++ vista/user/vista/resumen.php
@@ -138,8 +138,11 @@
 				<h1 style="color:red; font-size:40px; margin:0;"><?php echo $row1["evt_desc"]?></h1>
 				<h2>Fecha: <?php echo $row1["evt_fec_evento"]?></h2>
 				<h2>Direccion: <?php echo $row1["evt_direccion"]?></h2>
-				<form action="../controladores/php/califcacion.php" method="post" style="display:grid;  grid-template-columns: 1fr 1fr 1fr; margin-bottom:10px;">
+				<form action="../controladores/php/califcar.php" method="post" style="display:grid;  grid-template-columns: 1fr 1fr 1fr; margin-bottom:10px;">
 					<h2 style="margin:0;">Calificar</h2>
+					<input type="text" name="cali" value="<?php echo $row1["evt_calificacion"] ?>" hidden="hidden">
+					<input type="text" name="idcal" value="<?php echo $row1["evt_id"] ?>" hidden="hidden">
+					<input type="text" name="user" value="<?php echo $codigo ?>" hidden="hidden">
 					<select name="cal" id="cal">
 						<option value="1">1</option>
 						<option value="2">2</option>
