617065efbbbedcf9c2001877279ef57a6f0e00c8
vegabryam <vegabryam40@gmail.com>
2019-06-03 03:58:21 -0500

    creando mapa

--- vista/admin/vista/facturas.php
+++ vista/admin/vista/facturas.php
@@ -16,11 +16,7 @@
 		$row = $result->fetch_assoc();
 		
 		$sql1="SELECT *
-			  FROM T_FACTURA_CABECERA,
-			  	   T_FACTURA_DETALLE,
-				   T_EVENTOS
-			  WHERE fc_id = fd_fc_id AND
-			  		evt_id = fd_evt_id";
+			  FROM T_FACTURA_CABECERA";
 		
 		$result1 = $conn->query($sql1);
 	?>
@@ -93,27 +89,28 @@
                    <div class="detalle" style="background:white;">
                    	<table style="width:100%; color:black; text-align:center;">
                    		<tr>
-                   			<th style="border-bottom:1px solid black;">Nombre del Evento</th>
-                   			<th style="border-bottom:1px solid black;">Fecha del Evento</th>
-                   			<th style="border-bottom:1px solid black;">Descripcion</th>
-                   			<th style="border-bottom:1px solid black;">Cantidad</th>
-                   			<th style="border-bottom:1px solid black;">Total</th>
+                   			<th style="border-bottom:1px solid black;">N° Factura</th>
+                   			<th style="border-bottom:1px solid black;">fecha de Emisión</th>
+                   			<th style="border-bottom:1px solid black;">Estado</th>
+                   			<th style="border-bottom:1px solid black;">Ver</th>
                    			<th style="border-bottom:1px solid black;">Anular Factura</th>
                    		</tr>
                    		<?php
 							while($row1 = $result1->fetch_assoc()){
 								echo "<tr class='datos'>";
-									echo "<td>".$row1["evt_desc"]."</td>";
-									echo "<td>".$row1["evt_fec_evento"]."</td>";
-									echo "<td>".$row1["fd_desc"]."</td>";
-									echo "<td>".$row1["fd_cantidad"]."</td>";
-									echo "<td>$".$row1["fd_precio"]."</td>";
-									if($row1["fd_estado"] == 'N'){
-										echo "<td class='link_compra'><a href='../controladores/php/anular_factura.php?fc=".$row1["fc_id"]."&fd=".$row1["fd_id"]."&codigo=".$codigo."'><i class='fas fa-trash-alt' style='color:red;'></i></a></td>";
-									}else{
-										echo "<td>ANULADA</td>";
+									echo "<td>".$row1["fc_id"]."</td>";
+									echo "<td>".$row1["fc_fecha"]."</td>";
+									if($row1["fc_estado_entrega"] == 'ES'){
+										echo "<td>EN ESPERA</td>";
 									}
-									
+									if($row1["fc_estado_entrega"] == 'NP'){
+										echo "<td>EN PROGRESO</td>";
+									}
+									if($row1["fc_estado_entrega"] == 'R'){
+										echo "<td>RECIBIDO</td>";
+									}
+									echo "<td>"."<a href='ver_factura.php?fc=".$row1["fc_id"]."&codigo=".$row1["fc_usu_id"]."'>"."<i class='far fa-eye' style='color:greenyellow;'></i>"."</a>"."</td>";
+									echo "<td class='link_compra'><a href='../controladores/php/anular_factura.php?fc=".$row1["fc_id"]."&codigo=".$codigo."'><i class='fas fa-trash-alt' style='color:red;'></i></a></td>";
 								echo "</tr>";
 							}
 						?>
