5c3ad972b68c42434b15bbdff5bf1da0275b1bac
vegabryam <vegabryam40@gmail.com>
2019-06-03 05:38:57 -0500

    Crear empresa

---
+++ vista/admin/controladores/php/crear_empresa.php
@@ -0,0 +1,18 @@
+<?php
+	include '../../../../config/conexion.php';
+
+
+		$codigo = isset($_POST["codi"]) ? trim($_POST["codi"]): null;
+		$nombres =isset($_POST["nombres"]) ? mb_strtoupper(trim($_POST["nombres"]),"UTF-8"): null;
+		$cedula = isset($_POST["cedula"]) ? trim($_POST["cedula"]): null;
+		$direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]),"UTF-8"):null;
+		$telefono = isset($_POST["telefono"]) ? trim($_POST["telefono"]):null;
+		$estado = isset($_POST["ast"]) ? mb_strtoupper(trim($_POST["ast"]),"UTF-8"):null;
+
+
+		$sql = "ISNERT INTO T_EMPRESAS VALUES(0,'$cedula','$nombres','$direccion','$telefono','N',$estado)";
+
+		$result=$conn->query($sql);
+
+		header("Location: ../../vista/empresa.php?codigo=".$codigo);
+?>
