894f66bfd27c649f2635b01a50dab1cf7155056f
vegabryam <vegabryam40@gmail.com>
2019-06-03 06:17:00 -0500

    Crear empresa

--- vista/user/vista/resumen.php
+++ vista/user/vista/resumen.php
@@ -138,7 +138,7 @@
 				<h1 style="color:red; font-size:40px; margin:0;"><?php echo $row1["evt_desc"]?></h1>
 				<h2>Fecha: <?php echo $row1["evt_fec_evento"]?></h2>
 				<h2>Direccion: <?php echo $row1["evt_direccion"]?></h2>
-				<form action="../controladores/php/califcacion.php" method="post" style="display:grid;  grid-template-columns: 1fr 1fr; margin-bottom:10px;">
+				<form action="../controladores/php/califcacion.php" method="post" style="display:grid;  grid-template-columns: 1fr 1fr 1fr; margin-bottom:10px;">
 					<h2 style="margin:0;">Calificar</h2>
 					<select name="cal" id="cal">
 						<option value="1">1</option>
@@ -147,7 +147,7 @@
 						<option value="4">4</option>
 						<option value="5">5</option>
 					</select>	
-                    <input type="submit" value="Calificar" class="button" id="botonA">
+                    <input type="submit" value="Calificar" class="button" id="botonA" style="color:white; border:none; background:red; margin:0 10px; text-align:center;">
 				</form>
 				<?php
 					for ($i = 1; $i <= 5; $i++) {
