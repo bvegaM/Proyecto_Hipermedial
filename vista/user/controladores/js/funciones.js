743849cb034ab4a1a7cdbafe86fe47cf1d7d7499
vegabryam <vegabryam40@gmail.com>
2019-06-02 19:58:40 -0500

    creando js

--- vista/user/controladores/js/funciones.js
+++ vista/user/controladores/js/funciones.js
@@ -13,7 +13,7 @@ function cambiarCantidad(){
 		var pu = Number(document.getElementById("pU1").value)
 		pT1 = pu*c
 		document.getElementById("c1").innerHTML=c
-		document.getElementById("p1").innerHTML="$"+pT
+		document.getElementById("p1").innerHTML="$"+pT1
 		document.getElementById("cF1").value=c
 		document.getElementById("d1").style.visibility="visible";
 	}
@@ -21,7 +21,7 @@ function cambiarCantidad(){
 		var pT2 =Number(document.getElementById("pT2").value)
 		var pu = Number(document.getElementById("pU2").value)
 		pT2 = pu*c
-		document.getElementById("p2").innerHTML="$"+pT
+		document.getElementById("p2").innerHTML="$"+pT2
 		document.getElementById("c2").innerHTML=c
 		document.getElementById("cF2").value=c
 		document.getElementById("d2").style.visibility="visible";
@@ -30,7 +30,7 @@ function cambiarCantidad(){
 		var pT3 =Number(document.getElementById("pT3").value)
 		var pu = Number(document.getElementById("pU3").value)
 		pT3 = pu*c
-		document.getElementById("p3").innerHTML="$"+pT
+		document.getElementById("p3").innerHTML="$"+pT3
 		document.getElementById("c3").innerHTML=c
 		document.getElementById("cF3").value=c
 		document.getElementById("d3").style.visibility="visible";
@@ -39,7 +39,7 @@ function cambiarCantidad(){
 		var pT4 =Number(document.getElementById("pT3").value)
 		var pu = Number(document.getElementById("pU4").value)
 		pT4 = pu*c
-		document.getElementById("p4").innerHTML="$"+pT
+		document.getElementById("p4").innerHTML="$"+pT4
 		document.getElementById("c4").innerHTML=c
 		document.getElementById("pT4").value=pT
 		document.getElementById("cF4").value=c
@@ -49,7 +49,7 @@ function cambiarCantidad(){
 		var pT5 =Number(document.getElementById("pT3").value)
 		var pu = Number(document.getElementById("pU5").value)
 		pT5 = pu*c
-		document.getElementById("p5").innerHTML="$"+pT
+		document.getElementById("p5").innerHTML="$"+pT5
 		document.getElementById("c5").innerHTML=c
 		document.getElementById("pT5").value=pT
 		document.getElementById("cF5").value=c
